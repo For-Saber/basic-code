c799c152e7fd050a350a47f7c70a76146fd0f0d2
ForSaber <1300966363@qq.com>
2023-05-03 16:52:45 +0800

    Initial commit

---
+++ OopCode/src/com/itheima/phone/Phone.java
@@ -0,0 +1,18 @@
+package com.itheima.phone;
+
+public class Phone {
+
+    //属性
+    String brand;
+    double price;
+
+    //行为（方法）
+
+    public void call(){
+        System.out.println("手机在打电话。");
+    }
+
+    public void playGame(){
+        System.out.println("玩游戏。");
+    }
+}
---
+++ OopCode/src/com/itheima/phone/PhoneTest.java
@@ -0,0 +1,10 @@
+package com.itheima.phone;
+
+public class PhoneTest {
+    public static void main(String[] args) {
+
+        Phone p=new Phone();
+
+        p.call();
+    }
+}
