4355cba7a6eb7e71b6175a33d115d21fb1853380
ForSaber <1300966363@qq.com>
2023-05-03 12:04:37 +0800

    Initial commit

--- JavaDay07/src/com/itheima/test/Test09.java
+++ JavaDay07/src/com/itheima/test/Test09.java
@@ -11,32 +11,41 @@ public class Test09 {
                 {11, 66, 99}
         };
 
-        int sum1 = 0;
-        int sum2 = 0;
-        int sum3 = 0;
-        int sum4 = 0;
+//        int sum1 = 0;
+//        int sum2 = 0;
+//        int sum3 = 0;
+//        int sum4 = 0;
 
-        for (int i = 0; i < 3; i++) sum1 += arr[0][i];
-        for (int i = 0; i < 3; i++) sum2 += arr[1][i];
-        for (int i = 0; i < 3; i++) sum3 += arr[2][i];
-        for (int i = 0; i < 3; i++) sum4 += arr[3][i];
-
-/*
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 3; j++) {
-                sum += arr[i][j];
-            }
+//        for (int i = 0; i < 3; i++) sum1 += arr[0][i];
+//        for (int i = 0; i < 3; i++) sum2 += arr[1][i];
+//        for (int i = 0; i < 3; i++) sum3 += arr[2][i];
+//        for (int i = 0; i < 3; i++) sum4 += arr[3][i];
+//
+// for (int i = 0; i < 4; i++) {
+//     for (int j = 0; j < 3; j++) {
+//         sum += arr[i][j];
+//     }
+// }
+//        int sum = sum1 + sum2 + sum3 + sum4;
+        int yearSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int[] quarterArr = arr[i];
+            int quarterSum = getSum(quarterArr);
+            System.out.println("第" + (i + 1) + "季度的营业额为：" + quarterSum);
+            yearSum+=quarterSum;
         }
-*/
-        int sum = sum1 + sum2 + sum3 + sum4;
+        System.out.println("年度总营业额为："+yearSum);
 
+    }
 
-        System.out.println("第一季度营业额为" + sum1);
-        System.out.println("第二季度营业额为" + sum2);
-        System.out.println("第三季度营业额为" + sum3);
-        System.out.println("第四季度营业额为" + sum4);
-        System.out.println("总营业额为" + sum);
+    public static int getSum(int[] arr) {
 
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int j = arr[i];
+            sum += j;
+        }
+        return sum;
     }
 }
 
