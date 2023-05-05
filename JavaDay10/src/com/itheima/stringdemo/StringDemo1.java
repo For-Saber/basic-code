2c3e2b117996d1585768f9a63d35c6a787a3a52c
ForSaber <1300966363@qq.com>
2023-05-05 17:05:26 +0800

    对象数组练习

---
+++ JavaDay10/src/com/itheima/stringdemo/StringDemo1.java
@@ -0,0 +1,15 @@
+package com.itheima.stringdemo;
+
+public class StringDemo1 {
+    public static void main(String[] args) {
+
+        String s1="abc";
+
+        String s2=new String();
+
+        String s3=new String("abc");
+
+        char[] chs={'a','b','c','d'};
+        String s4=new String(chs);
+    }
+}
