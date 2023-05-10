ed4af716250a6f1c54be1f228f052e1d3826cb67
ForSaber <1300966363@qq.com>
2023-05-10 18:08:10 +0800

    学生管理系统demo

--- JavaDay11/src/com/itheima/studentsystem/StudentSystem.java
+++ JavaDay11/src/com/itheima/studentsystem/StudentSystem.java
@@ -1,21 +1,161 @@
 package com.itheima.studentsystem;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentSystem {
 
     public static void main(String[] args) {
 
-        while (true){
+        ArrayList<Student> list = new ArrayList<>();
+
+        while (true) {
+            mainMenu();
+            selectOptions();
 
 
         }
     }
-    public static void mainMenu(){
 
+    public static void mainMenu() {
+
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));//伪清屏
         System.out.println("------------------学生管理系统------------------");
         System.out.println("1.添加学生");
         System.out.println("2.删除学生");
         System.out.println("3.修改学生");
         System.out.println("4.查看学生");
-        System.out.println("");
+        System.out.println("按0退出系统");
+
     }
+
+    public static void selectOptions() {
+
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        switch (c) {
+            //TODO
+            //TODO
+            //TODO
+            //TODO
+            case '0' -> System.exit(0);
+        }
+    }
+
+    public static void createList() {
+
+    }
+
+    public static void addStudent(ArrayList<Student> list) {
+
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));//伪清屏
+        Scanner sc = new Scanner(System.in);
+        Student stu = new Student();
+
+        //先查询是否已存在
+        String newSid;
+
+        while (true) {
+            System.out.println("请输入要添加的学生ID：");
+            newSid = sc.next();
+            if (contains(list, newSid)) System.out.println("学生ID已存在，请重新输入！");
+            else break;
+        }
+
+        stu.setName(newSid);
+
+        System.out.println("请输入要添加的学生姓名：");
+        String newName = sc.next();
+        stu.setName(newName);
+        System.out.println("请输入要添加的学生年龄：");
+        int newAge = sc.nextInt();
+        stu.setAge(newAge);
+        System.out.println("请输入要添加的学生所在地：");
+        String newAddress = sc.next();
+        stu.setAddress(newAddress);
+
+        list.add(stu);
+        System.out.println("添加成功！");
+
+    }
+
+    public static void deleteStudent(ArrayList<Student> list, String deleteSid) {
+
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));//伪清屏
+        System.out.println("请输入要删除的学生ID：");
+
+        if (contains(list, deleteSid)) {
+            for (int i = 0; i < list.size(); i++) {
+                Student stu = list.get(i);
+                list.remove(stu);
+                System.out.println("删除成功！");
+            }
+        }
+        else System.out.println("没有查询到对应的学生ID！");
+    }
+
+    public static void editStudent(ArrayList<Student> list) {
+
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));//伪清屏
+        System.out.println("请输入要修改的学生ID：");
+        Scanner sc=new Scanner(System.in);
+        String sid = sc.next();
+        if (contains(list,sid)){
+
+            Student stu=new Student();
+            stu.setName(sid);
+            System.out.println("请输入新的学生姓名：");
+            String newName = sc.next();
+            stu.setName(newName);
+            System.out.println("请输入新的学生年龄：");
+            int newAge = sc.nextInt();
+            stu.setAge(newAge);
+            System.out.println("请输入新的学生所在地：");
+            String newAddress = sc.next();
+            stu.setAddress(newAddress);
+
+            int index=getIndexByID(list,sid);
+            list.set(index,stu);
+
+        }
+
+        else System.out.println("没有查询到对应的学生ID！");
+    }
+
+    public static void getAllInfo(ArrayList<Student> list) {
+
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));//伪清屏
+        System.out.println("以下是所有学生信息：");
+        for (int i = 0; i < list.size(); i++) {
+
+            Student stu = list.get(i);
+
+            System.out.println(
+                    "学生ID：" + stu.getSid() + " " +
+                            "学生姓名：" + stu.getName() + " " +
+                            "学生年龄：" + stu.getAge() + " " +
+                            "学生所在地：" + stu.getAddress());
+
+        }
+
+        System.out.println("\n查询完毕。");
+
+    }
+
+    public static boolean contains(ArrayList<Student> list, String sid) {
+        for (int i = 0; i < list.size(); i++) {
+            Student stu = list.get(i);
+            if (stu.getSid().equals(sid)) return true;
+        }
+        return false;
+    }
+
+    public static int getIndexByID(ArrayList<Student> list, String sid) {
+        for (int i = 0; i < list.size(); i++) {
+            Student stu = list.get(i);
+            if (stu.getSid().equals(sid)) return i;
+        }
+        return -1;
+    }
+
 }
---
+++ JavaDay11/src/com/itheima/studentsystem/Test.java
@@ -0,0 +1,11 @@
+package com.itheima.studentsystem;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("123");
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));
+        System.out.println("123");
+    }
+}
